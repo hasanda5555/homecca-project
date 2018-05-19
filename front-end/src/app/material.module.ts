ff8f3b62bbc546d8d744d0c71d362219dfa7774d
hasanda5555 <hasanda5555@yahoo.com>
2018-05-19 15:54:21 +1000

    More Ui updates

--- front-end/src/app/material.module.ts
+++ front-end/src/app/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatDialogModule ,MatIconModule} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule,MatInputModule, MatProgressSpinnerModule, MatCardModule],
-  exports: [MatButtonModule, MatToolbarModule,MatInputModule, MatProgressSpinnerModule, MatCardModule],
+  imports: [MatButtonModule, MatToolbarModule,MatInputModule, MatProgressSpinnerModule, MatCardModule, MatDialogModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule,MatInputModule, MatProgressSpinnerModule, MatCardModule, MatDialogModule, MatIconModule],
 })
 export class MaterialModule {
 
