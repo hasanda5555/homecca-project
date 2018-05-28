82984a8b45d071ed880995780725d8a3cca93c07
Hasanda Fernando <hfernando@bglcorp.com.au>
2018-05-28 14:26:57 +1000

    email server with Async function

--- back-end/src/main/java/com/social/controller/AccountController.java
+++ back-end/src/main/java/com/social/controller/AccountController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- back-end/src/main/java/com/social/services/NotificationServices.java
+++ back-end/src/main/java/com/social/services/NotificationServices.java
@@ -5,29 +5,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationServices {
 
+    @Autowired
     private JavaMailSender javaMailSender;
 
-    @Autowired
-    public NotificationServices(JavaMailSender javaMailSender){
-        this.javaMailSender = javaMailSender;
-    }
 
+    @Async
     public void sendNotification(User user) throws MailException{
 
         //send email
         SimpleMailMessage mail = new SimpleMailMessage();
 
         mail.setTo(user.getUsername());
-        mail.setFrom("gentoo5555@gmail.com");
+        mail.setFrom("musicsharelib@gmail.com");
         mail.setSubject("Hommeca Mail");
         mail.setText("This is test email from homeeca");
 
         javaMailSender.send(mail);
-
+        System.out.println("java mail sender : " + javaMailSender.toString());
     }
 }
